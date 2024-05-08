64cb308dfc40ae86dda1ff27ba989cff0f35c78d
acej0k3r <Ace.rav3n@gmail.com>
2024-05-08 03:04:42 -0400

    quick vercel update

--- component/Footer.jsx
+++ component/Footer.jsx
@@ -4,18 +4,14 @@ import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div className='footer-container'>
-      <p  >
-        2022 AceJokerCapital INC. All rights reserved
+    <div className="footer-container">
+      <p>2022 Md A Faisal. All rights reserved</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
       </p>
-      <p className='icons' >
-        <AiFillInstagram  />
-        <AiOutlineTwitter  />
-        
-      </p>
-      
     </div>
-  )
+  );
 }
 
 export default Footer
